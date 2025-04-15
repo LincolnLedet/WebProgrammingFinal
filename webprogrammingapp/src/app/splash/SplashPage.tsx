5dfc681b42c74408508b27362017c021cff46451
Kevin Zburlea <kevzbuu@gmail.com>
2025-04-14 21:58:14 -0400

    Updated splash page, added button to go from splashpage to home, fixed topbar, changed some CSS, still needs to be set as first page seen when opening website (not sure how to set it up)

--- webprogrammingapp/src/app/splash/SplashPage.tsx
+++ webprogrammingapp/src/app/splash/SplashPage.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import TopBar from '../components/TopBar';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -8,9 +8,11 @@ import dawglogo from '../images/dawglogo.png'
 import temp from '../images/tempG.png';
 
 export default function SplashPage() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className={styles.main_body}>
-      <TopBar isLoggedIn={true} title="DAWGIFY" changeLogStatus={() => {}} />
+      <TopBar isLoggedIn={isLoggedIn} title="DAWGIFY" changeLogStatus={setIsLoggedIn} />
       <hr></hr>
 
       {/* Three Columns */}
@@ -26,6 +28,9 @@ export default function SplashPage() {
           <p className={styles.col2_content}>
           is a UGA student community playlist sharer where students can view their own Spotify accounts information and create and edit playlist to share with others.
           </p>
+          <Link href="/">
+            <button className={styles.getStartedButton}> Get Started</button>
+          </Link>
         </div>
 
         <div className={styles.column}>
